e8bc03924f37f30d647aa6c5b9c4932ae3501bca
SharonAliyas5573 <116248803+SharonAliyas5573@users.noreply.github.com>
2022-11-21 20:03:28 +0530

    .......

--- Home.py
+++ Home.py
@@ -3,6 +3,6 @@ from flask import Flask ,url_for,render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('page2.html')
 
 app.run(debug=True)
