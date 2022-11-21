24809a9f74fa2513f932c7f528157515fd2e7395
SharonAliyas5573 <116248803+SharonAliyas5573@users.noreply.github.com>
2022-11-21 22:08:46 +0530

    ......

--- Home.py
+++ Home.py
@@ -1,8 +1,17 @@
-from flask import Flask ,url_for,render_template
+from flask import Flask ,url_for,render_template,request,redirect,flash
 
 app = Flask(__name__)
-@app.route('/')
+
+
+@app.route('/',methods =['GET','POST'])
 def home():
+    return render_template('home.html')
+
+@app.route('/options',methods =['GET','POST'])
+
+def options():
+    value = request.form.get('FLAVOURS')
+    print(value)
     return render_template('page2.html')
 
 app.run(debug=True)
