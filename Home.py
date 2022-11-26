5a51f146ea1705b9e53b40b4d8cd9eef516cf0b0
SharonAliyas5573 <116248803+SharonAliyas5573@users.noreply.github.com>
2022-11-26 13:21:57 +0530

    .......

--- Home.py
+++ Home.py
@@ -4,8 +4,11 @@ from flask import Flask ,url_for,render_template,redirect,request
 
 app = Flask(__name__)
 
+@app.route('/',methods=['POST','GET'])
+def root():
+    return redirect('/home')
 
-@app.route('/',methods =['GET','POST'])
+@app.route('/home')
 def home():
     return render_template('home.html')
 @app.route('/order',methods =['GET','POST'])
@@ -18,7 +21,7 @@ def form():
     FLAVOUR ={"1":"Strawberry","2":"Vanilla","3":"Chocolate"}
     FLAVOUR_PRICE ={'1':120,'2':90,'3':110}
     TOPPING ={"1":"Sugar crystals","2":"M&M's","3":"Oreo","4":"Brownies","5":"Chocolate Syrup","6":"Chocolate Chips"}
-    TOPPING_PRICE ={'1':'0','2':'35','3':'25','4':'25','5':'25','6':'30'}
+    TOPPING_PRICE ={'1':0,'2':35,'3':25,'4':25,'5':25,'6':30}
     BILL=0
     f = request.form.get('FLAVOURS')
     s = request.form.get('SIZE')
@@ -37,31 +40,32 @@ def form():
     
     topping=[]
     topping_price=[]
-    for i in t:
-        topping.append(TOPPING[i])
+    for i in t:                                        
+        topping.append(TOPPING[i])                   #LIST OF TOPPING
 
   
     for j in t:
-        topping_price.append(TOPPING_PRICE[j])
-
-    for i in range(0, len(topping_price)):
-        topping_price[i] = int(topping_price[i]) 
-
-    total_toppings_price=0
-    for k in range(0,len(topping_price)):
-        total_toppings_price= total_toppings_price +topping_price[k]
-
+        topping_price.append(TOPPING_PRICE[j])       #list of Topping price
         
 
-    Topping = ', '.join(topping)
+    
+    total_toppings_price=0
+    for k in range(0,len(topping_price)):             ## SUM OF TOPPING PRICE
+        total_toppings_price= total_toppings_price +topping_price[k]
+
+    topping_price=[str(x) for x in topping_price]  #convert topping price list to int
    
+    temp=''
+    for i in range(len(topping)):
+        temp += " " +topping[i] +" ("+ topping_price[i]+") "
+
     BILL= BILL+total_toppings_price+f_price
 
     
-
+   
         
    
-    return render_template('page3.html',BILL=BILL,f=f,Topping=Topping,s=s)
+    return render_template('page3.html',BILL=BILL,f=f,s=s,t=temp)
 
 
 app.run(debug=True)
